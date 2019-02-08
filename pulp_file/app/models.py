01fe0398b14998210e452cf1c35faf7638fde7b5
Dennis Kliban <dkliban@redhat.com>
2019-02-08 15:36:30 -0500

    pass through cache implementation

--- pulp_file/app/models.py
+++ pulp_file/app/models.py
@@ -1,4 +1,6 @@
+from gettext import gettext as _
 from logging import getLogger
+from os import path
 
 from django.db import models
 
@@ -31,6 +33,14 @@ class FileContent(Content):
             'digest'
         )
 
+    @staticmethod
+    def init_from_relative_path(relative_path):
+        """
+
+        :param relative_path:
+        :return:
+        """
+        
 
 class FileRemote(Remote):
     """
@@ -38,6 +48,39 @@ class FileRemote(Remote):
     """
 
     TYPE = 'file'
+
+    def get_remote_artifact_url(self, relative_path=None):
+        """
+        Get the full URL for a RemoteArtifact from a relative path.
+
+        This method returns the URL for a RemoteArtifact by concatinating the Remote's url and the
+        relative path.located in the Remote. Plugin writers are expected to override this method
+        when a more complex algorithm is needed to determine the full URL.
+
+        Args:
+            relative_path (str): The relative path of a RemoteArtifact
+
+        Raises:
+            ValueError: If relative_path starts with a '/'.
+
+        Returns:
+            str: A URL for a RemoteArtifact available at the Remote.
+        """
+        if path.isabs(relative_path):
+            raise ValueError(_("Relative path can't start with '/'. {0}").format(relative_path))
+        return path.join(self.url, relative_path)
+
+    def get_remote_artifact_content_type(self, relative_path=None):
+        """
+        Get the type of content that should be available at the relative path.
+
+        Args:
+            relative_path (str): The relative path of a RemoteArtifact
+
+        Returns:
+            Class: FileContent
+        """
+        return FileContent
 
 
 class FilePublisher(Publisher):
